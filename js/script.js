8a4e8d55aed4bd98fe4f1ba2ab4a29235e714cb3
KrzysztofSiek <krzysztof.siek01@gmail.com>
2022-02-24 08:25:44 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -156,5 +156,7 @@ window.onload = function() {
   recaptcha.required = true;
   recaptcha.oninvalid = function(e) {
     alert("Please check the box, \"I'm not a robot\" in the reCaptcha below.");
+    const error = document.querySelector('.recaptha');
+    error.classList.add('show');
   }
 }
