4e3d3085a73d0c064f6fc6f41dc7baf7882d930f
KrzysztofSiek <krzysztof.siek01@gmail.com>
2022-02-24 08:21:26 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -148,13 +148,13 @@ window.addEventListener('load', (event) => {
 
 
     resetText();
-
-      
-    // var recaptcha = document.forms["contact-form"]["g-recaptcha-response"];
-    // recaptcha.required = true;
-    // recaptcha.oninvalid = function(e) {
-    //   alert("Please check the box, \"I'm not a robot\" in the reCaptcha below.");
-    // }
-   
-
 });
+
+
+window.onload = function() {
+  var recaptcha = document.forms["contact-form"]["g-recaptcha-response"];
+  recaptcha.required = true;
+  recaptcha.oninvalid = function(e) {
+    alert("Please check the box, \"I'm not a robot\" in the reCaptcha below.");
+  }
+}
